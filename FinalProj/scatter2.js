f15fc1168ff5e011d7e364229697b6a1d8a4bbbb
Abhishek M Iyengar <abhi.ingr@gmail.com>
2016-12-02 13:09:39 -0500

    Add files via upload

---
+++ FinalProj/scatter2.js
@@ -0,0 +1,158 @@
+var margin = { top: 20, right: 300, bottom: 50, left: 250 },
+    outerWidth = 1200,
+    outerHeight = 500,
+    width = outerWidth - margin.left - margin.right,
+    height = outerHeight - margin.top - margin.bottom;
+
+var x = d3.scale.linear()
+    .range([0, width]).nice();
+
+var y = d3.scale.linear()
+    .range([height, 0]).nice();
+
+var xCat = "Energy Usage",
+    yCat = "CO2 Emissions",
+    rCat = "Size",
+    mCat = "Year of Year",
+    colorCat = "Region";
+
+d3.csv("cereal2.csv", function(data) {
+  data.forEach(function(d) {
+    d["Year of Year"] = +d["Year of Year"];
+    d.Size = +d.Size;
+
+  });
+
+  var xMax = d3.max(data, function(d) { return d[xCat]; }) * 1.05,
+      xMin = d3.min(data, function(d) { return d[xCat]; }) * 0,
+
+      yMax = d3.max(data, function(d) { return d[yCat]; }) * 1.05,
+      yMin = d3.min(data, function(d) { return d[yCat]; }) * -0.2;
+
+
+  x.domain([xMin, xMax]);
+  y.domain([yMin, yMax]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom")
+      .tickSize(-height);
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .tickSize(-width);
+
+  var color = d3.scale.category10();
+
+  var tip = d3.tip()
+      .attr("class", "d3-tip")
+      .offset([-10, 0])
+      .html(function(d) {
+        return xCat + ": " + d[xCat] + "<br>" + yCat + ": " + d[yCat] + "<br>" + mCat + ": " + d[mCat] + "<br>" + colorCat + ":" + d[colorCat];
+      });
+
+  var zoomBeh = d3.behavior.zoom()
+      .x(x)
+      .y(y)
+      .scaleExtent([0, 2000])
+      .on("zoom", zoom);
+
+  var svg = d3.select("#scatter")
+    .append("svg")
+      .attr("width", outerWidth)
+      .attr("height", outerHeight)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+      .call(zoomBeh);
+
+  svg.call(tip);
+
+  svg.append("rect")
+      .attr("width", width)
+      .attr("height", height);
+
+  svg.append("g")
+      .classed("x axis", true)
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .append("text")
+      .classed("label", true)
+      .attr("x", width)
+      .attr("y", margin.bottom - 10)
+      .style("text-anchor", "end")
+      .text(xCat);
+
+  svg.append("g")
+      .classed("y axis", true)
+      .call(yAxis)
+    .append("text")
+      .classed("label", true)
+      .attr("transform", "rotate(-90)")
+      .attr("y", -margin.left + 150)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text(yCat);
+
+  var objects = svg.append("svg")
+      .classed("objects", true)
+      .attr("width", width + 20)
+      .attr("height", height);
+
+  objects.append("svg:line")
+      .classed("axisLine hAxisLine", true)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", width)
+      .attr("y2", 0)
+      .attr("transform", "translate(0," + height * 10 + ")");
+
+  objects.append("svg:line")
+      .classed("axisLine vAxisLine", true)
+      .attr("x1", 0)
+      .attr("y1", 0)
+      .attr("x2", 0)
+      .attr("y2", height);
+
+  objects.selectAll(".dot")
+      .data(data)
+    .enter().append("circle")
+      .classed("dot", true)
+      .attr("r", function (d) {console.log(rCat);
+        return 12 * Math.sqrt(d[rCat] / Math.PI); })
+      .attr("transform", transform)
+      .style("fill", function(d) { return color(d[colorCat]); })
+      //.attr("fill","none")
+      .on("mouseover", tip.show)
+      .on("mouseout", tip.hide);
+
+  var legend = svg.selectAll(".legend")
+      .data(color.domain())
+    .enter().append("g")
+      .classed("legend", true)
+      .attr("transform", function(d, i) { return "translate(0," + i * 50 + ")"; });
+
+  legend.append("circle")
+      .attr("r", 6)
+      .attr("stroke-width",2)
+      .attr("cx", width + 20)
+      .attr("fill", color);
+
+  legend.append("text")
+      .attr("x", width + 50)
+      .attr("font-size","25px")
+      .text(function(d) { return d; });
+  
+
+  function zoom() {
+    svg.select(".x.axis").call(xAxis);
+    svg.select(".y.axis").call(yAxis);
+
+    svg.selectAll(".dot")
+        .attr("transform", transform);
+  }
+
+  function transform(d) {
+    return "translate(" + x(d[xCat]) + "," + y(d[yCat]) + ")";
+  }
+});

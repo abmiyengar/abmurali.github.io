3dc7951b65b707dbcfa24991251eff771ee4fc30
Abhishek M Iyengar <abhi.ingr@gmail.com>
2017-02-18 15:48:54 -0500

    Update scatter2.js

--- FinalProj/scatter2.js
+++ FinalProj/scatter2.js
@@ -1,5 +1,5 @@
 var margin = { top: 20, right: 300, bottom: 50, left: 100 },
-    outerWidth = 500,
+    outerWidth = 700,
     outerHeight = 250,
     width = outerWidth - margin.left - margin.right,
     height = outerHeight - margin.top - margin.bottom;
